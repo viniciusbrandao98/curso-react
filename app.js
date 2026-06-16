0cd077b65db09fd31a273739fcf4c87e0095e8c2
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-06-16 11:30:35 -0300

    feat: introduzindo o JSX e renderizando elementos básicos

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
+
+function nomeCompleto(usuario) {
+    return usuario.nome + ' ' + usuario.sobrenome
+}
+
+const usuario = {
+    nome: "Vinícius",
+    sobrenome: "Brandão"
+}
+
+const nome = "Vinícius Brandão";
+const pagina = (
+    <div>
+        <h1 className="header">Olá {nome}!</h1>
+        <p>Isso é um parágrafo</p>
+    </div>
+);
+
 //Básico do React 
-ReactDOM.render(<p>Isso é um paragrafo</p>, document.querySelector("#root"));
+ReactDOM.render(
+    pagina,
+    document.querySelector("#root")
+);
