cdf2d5e01995f97db13aeec13cbdeec382c3c9be
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-06-16 16:14:49 -0300

    feat: Diferença entre componente funcional e de classe

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
+
+//Componente funcional
 function BemVindo(props) {
     return <h1>Bem vindo{props.nome}!</h1>
 }
 
-function Conteudo() {
-    return (
-        <div>
-            <p>Isso é uma frase aleatoria so pra ocupar espaço</p>
-            <p>Outra frase pra ocupar espaço blablabla</p>
-        </div>
-    )
-}
-
+//function Conteudo() {
+//    return (
+//        <div>
+//            <p>Isso é uma frase aleatoria so pra ocupar espaço</p>
+//            <p>Outra frase pra ocupar espaço blablabla</p>
+//        </div>
+//    )
+//}
 
 function App() {
     return (
         <div>
             <BemVindo nome=" Vinícius" />
             <BemVindo nome=" Isla" />
-            <Conteudo />
+            <Conteudo/>
         </div>
     )
 }
+
+//Componente de classe
+class Conteudo extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+        <div>
+           <p>Isso é uma frase aleatoria so pra ocupar espaço</p>
+            <p>Outra frase pra ocupar espaço blablabla</p>
+        </div>
+    )
+    }
+}
+
 //Básico do React 
 ReactDOM.render(
     <App/>,
